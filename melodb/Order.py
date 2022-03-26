aa03b6f6bc7b7483be64122a4b859e7f6b32a11e
omar <omarboukhris@gmail.com>
2022-03-26 18:03:21 +0100

    added empty trade builder

--- melodb/Order.py
+++ melodb/Order.py
@@ -3,7 +3,7 @@ from loggers import ILogger
 
 class Order:
 
-	def __init__(self, order: dict, logger: ILogger = ILogger("Order_DataClass")):
+	def __init__(self, logger: ILogger = ILogger("Order_DataClass"), **order):
 		self.logger = logger
 		if logger is None:
 			self.logger = ILogger("Order_DataClass")
@@ -14,12 +14,29 @@ class Order:
 		self.status = order["status"]
 		self.symbol = order["symbol"]
 		self.instrument = order["instrument"]
-		self.order_type = order["type"]
+		self.order_type = order["order_type"]
 		self.side = order["side"]
 		self.price = order["price"]
 		self.quantity = order["quantity"]
 		self.open_ts = order["open_ts"]
 		self.close_ts = order["close_ts"]
+		self.forecast = order["forecast"]
+
+	@staticmethod
+	def empty():
+		return Order(
+			order_id=0,
+			status="open",
+			symbol="",
+			instrument="",
+			order_type="",
+			side="",
+			price=0.,
+			quantity=0,
+			open_ts="",
+			close_ts="",
+			forecast=0.
+		)
 
 	@staticmethod
 	def labels():
